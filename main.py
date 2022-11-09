fca1fc197285e26480bdefa58c73377913e28a2e
artS3rg <59173918+artS3rg@users.noreply.github.com>
2022-11-09 21:16:55 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import config as cfg
 import aiogram as a
 import logging
+import keyboards as k
 from db import BotDB
-from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.dispatcher import FSMContext
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot = a.Bot(token=cfg.TOKEN, parse_mode="HTML")
@@ -14,46 +13,22 @@ start_mess = 'Приветствую тебя, пользователь!\nЭто
              'потеряться в 4 стенах :)\nТакже не забудь подписаться на группу Московского Политеха: \nt.me/mospolytech '
 
 
-class PrNavigation(StatesGroup):
-    pr_point_b = State()
-
-
-class BsNavigation(StatesGroup):
-    bs_point_b_input = State()
-    bs_point_b_audit = State()
-
-
-start_buttons = ["🔻Маршрут", "📖Полезная информация", "🆕Предложить идею"]
-start_keyboard = a.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-start_keyboard.add(*start_buttons)
-
-navigation_buttons = ["Большая Семёновская (БС)", "↪️Назад"]
-navigation_keyboard = a.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
-navigation_keyboard.add(*navigation_buttons)
-
-
 @dp.message_handler(commands='start')
 async def start(mess: a.types.Message):
     if not (BotDB.user_exists(mess.from_user.id)):
         BotDB.add_user(mess.from_user.id, mess.from_user.full_name)
-        await mess.bot.send_message(mess.from_user.id, start_mess, reply_markup=start_keyboard)
+        await mess.bot.send_message(mess.from_user.id, start_mess, reply_markup=k.start_keyboard)
     else:
-        await mess.bot.send_message(mess.from_user.id, 'Добро пожаловать!', reply_markup=start_keyboard)
+        await mess.bot.send_message(mess.from_user.id, 'Добро пожаловать!', reply_markup=k.start_keyboard)
 
 
-@dp.message_handler(lambda message: message.text == "🔻Маршрут")
+@dp.message_handler(lambda message: message.text == "🗺Навигатор")
 async def navigation(mess: a.types.Message):
     await mess.bot.delete_message(mess.from_user.id, mess.message_id)
-    await mess.bot.send_message(mess.from_user.id, 'Выберите корпус', reply_markup=navigation_keyboard)
+    await mess.bot.send_message(mess.from_user.id, 'Выберите корпус', reply_markup=k.navigation_keyboard)
 
 
-@dp.message_handler(lambda message: message.text == "↪️Назад")
-async def back(mess: a.types.Message):
-    await mess.bot.delete_message(mess.from_user.id, mess.message_id)
-    await mess.bot.send_message(mess.from_user.id, 'Главный раздел', reply_markup=start_keyboard)
-
-
-@dp.message_handler(lambda message: message.text == "📖Полезная информация")
+@dp.message_handler(lambda message: message.text == "❓FAQ")
 async def info(mess: a.types.Message):
     a = '🔸<b>Адрес кампуса на Большой Семёновской:</b>\n' \
         'учебные корпуса «А», «Б», «В», «Н», «НД»\n' \
@@ -97,95 +72,11 @@ async def info(mess: a.types.Message):
     await mess.bot.send_message(mess.from_user.id, a)
 
 
-"""Пряники"""
-
-
-@dp.message_handler(lambda message: message.text == "Прянишникова (ПР)", state=None)
-async def pr_navigation(mess: a.types.Message):
-    buttons = [
-        a.types.InlineKeyboardButton(text="Вход", callback_data="pr_input"),
-        a.types.InlineKeyboardButton(text="Аудитория", callback_data="pr_audit"),
-    ]
-    keyboard = a.types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
+@dp.message_handler(lambda message: message.text == "🏠Главная")
+async def back_home(mess: a.types.Message):
     await mess.bot.delete_message(mess.from_user.id, mess.message_id)
-    await mess.bot.send_message(mess.from_user.id, 'Выберите начало маршрута', reply_markup=keyboard)
+    await mess.bot.send_message(mess.from_user.id, 'Главный раздел', reply_markup=k.start_keyboard)
 
-
-@dp.callback_query_handler(text='pr_input', state=None)
-async def pr_navigation_input(call: a.types.CallbackQuery):
-    await call.message.delete()
-    await call.bot.send_message(call.from_user.id, 'Введите номер нужной Вам аудитории')
-    await PrNavigation.pr_point_b.set()
-
-
-@dp.callback_query_handler(state=PrNavigation.pr_point_b)
-async def pr_nav_input_next(mess: a.types.Message, state: FSMContext):
-    await state.finish()
-
-
-@dp.callback_query_handler(text='pr_audit', state=None)
-async def pr_navigation_audit(call: a.types.CallbackQuery):
-    await call.message.delete()
-
-
-"""Пряники"""
-
-"""Большая Семён"""
-
-
-@dp.message_handler(lambda message: message.text == "Большая Семёновская (БС)", state=None)
-async def bs_navigation(mess: a.types.Message):
-    await mess.bot.delete_message(mess.from_user.id, mess.message_id)
-    buttons = [
-        a.types.InlineKeyboardButton(text="Вход", callback_data="bs_input"),
-        a.types.InlineKeyboardButton(text="Аудитория", callback_data="bs_audit"),
-    ]
-    keyboard = a.types.InlineKeyboardMarkup(row_width=2)
-    keyboard.add(*buttons)
-    await mess.bot.send_message(mess.from_user.id, 'Выберите начало маршрута', reply_markup=keyboard)
-
-
-@dp.callback_query_handler(text='bs_input', state=None)
-async def bs_nav_input(call: a.types.CallbackQuery):
-    await call.message.delete()
-    await call.bot.send_message(call.from_user.id, 'Введите номер нужной Вам аудитории')
-    await BsNavigation.bs_point_b_input.set()
-
-
-@dp.message_handler(state=BsNavigation.bs_point_b_input)
-async def bs_nav_input_next(mess: a.types.Message, state: FSMContext):
-    text = mess.text
-    campus_letters = ["А", "В", "Б", "Н"]
-    travel = ""
-    if (len(text) != 4) or (text[0] not in campus_letters) or (int(text[1]) > 4):
-        await mess.bot.send_message(mess.from_user.id, "Неверная аудитория. Начните заново")
-    else:
-        travel += "1. Пройдите к аудитории, показанной на карте"
-        match text[0]:
-            case "А":
-                pass
-            case "В":
-                pass
-            case "Б":
-                pass
-            case "Н":
-                pass
-    await mess.bot.send_message(mess.from_user.id, travel)
-    await state.finish()
-
-
-@dp.callback_query_handler(text='bs_audit', state=None)
-async def bs_marsh_audit(call: a.types.CallbackQuery):
-    pass
-
-
-@dp.message_handler(state=BsNavigation.bs_point_b_audit)
-async def bs_marsh_audit_next(mess: a.types.Message, state: FSMContext):
-    pass
-
-
-"""Большая Семён"""
 
 if __name__ == "__main__":
     from handlers import dp
