9ff57a7ae7f17cb8ca506981a2f3fd038492f2d1
Полина <lovepolina2004@gmail.com>
2022-12-05 00:57:31 +0300

    Добавила отправку фото

--- handlers/helpers.py
+++ handlers/helpers.py
@@ -2,6 +2,7 @@
 
 import aiogram as a
 import keyboards as k
+import os
 from main import dp, BotDB
 from aiogram import types
 from aiogram.dispatcher.filters import Text
@@ -27,17 +28,28 @@ async def send(mess: a.types.Message):
     await mess.bot.send_message(mess.from_user.id, "Выберите интересующий вас вопрос:", reply_markup=inline_questions)
 
 
+# Ответ
 @dp.callback_query_handler(Text(startswith='answer_'))
 async def send_answer(callback_query: types.CallbackQuery):
     answer_id = int(callback_query.data.split('_')[1])
     answer_text = BotDB.cursor.execute("SELECT text FROM information WHERE id = ?", (answer_id, )).fetchone()[0]
+
+    files = os.listdir('pictures')
+
     await callback_query.message.answer(answer_text)
+    album = types.MediaGroup()
+    for file in files:
+        if (callback_query.data + '_') in file:
+            path = 'pictures/' + file
+            photo = types.InputFile(path_or_bytesio=path)
+            album.attach_photo(photo=photo)
+    await callback_query.bot.send_media_group(chat_id=callback_query.from_user.id, media=album)
     await callback_query.answer()
 
 
 @dp.message_handler(lambda message: message.text == "🔎 Поиск")
 async def send(mess: a.types.Message):
-    await mess.bot.send_message(mess.from_user.id, 'А его пока нет ┐(￣ヮ￣)┌')
+    await mess.bot.send_message(mess.from_user.id, 'А его пока нет 🙃')
 
 
 @dp.message_handler(lambda message: message.text == "↩ Назад")
