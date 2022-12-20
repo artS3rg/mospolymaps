279347033db7de184aa5a75337d414300d794ed2
artS3rg <59173918+artS3rg@users.noreply.github.com>
2022-12-20 20:06:22 +0300

    обновочка подъехала

--- main.py
+++ main.py
@@ -15,17 +15,26 @@ start_mess = 'Приветствую тебя, пользователь!\nЭто
 
 @dp.message_handler(commands='start')
 async def start(mess: a.types.Message):
+    await mess.bot.delete_message(mess.from_user.id, mess.message_id)
     if not (BotDB.user_exists(mess.from_user.id)):
         BotDB.add_user(mess.from_user.id, mess.from_user.full_name)
         await mess.bot.send_message(mess.from_user.id, start_mess, reply_markup=k.start_keyboard)
     else:
-        await mess.bot.send_message(mess.from_user.id, 'Добро пожаловать!', reply_markup=k.start_keyboard)
+        status = BotDB.cursor.execute("SELECT status FROM users WHERE user_id = ?", (int(mess.from_user.id),)).fetchone()[0]
+        if status == 'ban':
+            await mess.bot.send_message(mess.from_user.id, "Вы забанены!")
+        else:
+            await mess.bot.send_message(mess.from_user.id, 'Добро пожаловать!', reply_markup=k.start_keyboard)
 
 
 @dp.message_handler(lambda message: message.text == "🗺 Навигатор")
 async def navigation(mess: a.types.Message):
     await mess.bot.delete_message(mess.from_user.id, mess.message_id)
-    await mess.bot.send_message(mess.from_user.id, 'Выберите корпус', reply_markup=k.navigation_keyboard)
+    status = BotDB.cursor.execute("SELECT status FROM users WHERE user_id = ?", (int(mess.from_user.id),)).fetchone()[0]
+    if status == 'ban':
+        await mess.bot.send_message(mess.from_user.id, "Вы забанены!")
+    else:
+        await mess.bot.send_message(mess.from_user.id, 'Выберите корпус', reply_markup=k.navigation_keyboard)
 
 
 @dp.message_handler(lambda message: message.text == "❓ FAQ")
